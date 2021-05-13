a47ef93b1af40b4c48cab3f2adb39b625bd69fc5
Azhe-kun <azhe403@gmail.com>
2021-05-13 13:15:08 +0700

    add missing semicolon

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,10 @@
 import { createConfig, start, stop } from './localtunnel';
 import { commands, ExtensionContext } from 'vscode';
 
-export const NAMESPACE = 'localtunnel-for-vscode'
+export const NAMESPACE = 'localtunnel-for-vscode';
 
 export function activate(context: ExtensionContext) {
-	context.subscriptions.push(commands.registerCommand(`${NAMESPACE}.create-config`, () => createConfig(context)))
+	context.subscriptions.push(commands.registerCommand(`${NAMESPACE}.create-config`, () => createConfig(context)));
 	context.subscriptions.push(commands.registerCommand(`${NAMESPACE}.start`, () => start(context)));
 	context.subscriptions.push(commands.registerCommand(`${NAMESPACE}.stop`, () => stop(context)));
 }
